7b4eb881b77d7feec6f301d9c0887fd4ee149162
Camille Mizony <cmizony@gmail.com>
2015-12-21 23:36:36 -0800

    Add form validation for routes
    **Problem**
    https://github.com/10alab/Siurana/issues/10
    It would be nice to get a warning message when adding a route in case we
    are about to create a duplicate route.
    
    **Solution**
    * Add Angular form validation for route name
    * Add location validation using Google service
    * Add icon color to confirm success/failure
    * Add icon tooltip informative message
    
    **Result**
    User has visual indicators to know if a route name is not unique

---
+++ test/unit/directives/routeName.js
@@ -0,0 +1,68 @@
+'use strict'
+
+describe('Directive: routeName', function() {
+  var element, scope, form, deferred, utilsRouteSvc
+
+  beforeEach(module('climbingMemo', function($provide) {
+    // Stub for utilsRouteSvc
+    utilsRouteSvc = {
+      getRoutes: function() {}
+    }
+    $provide.value('utilsRouteSvc', utilsRouteSvc)
+  }))
+
+  beforeEach(inject(function($rootScope, $compile, $q) {
+    scope = $rootScope
+    deferred = $q.defer()
+
+    spyOn(utilsRouteSvc, 'getRoutes').and.returnValue(deferred.promise)
+
+    element = angular.element(
+      '<form name="form">' +
+      '<input ng-model="model.test" route-name name="test">' +
+      '</form>'
+    )
+    scope.model = { test: null }
+    $compile(element)(scope)
+    form = scope.form
+  }))
+
+  it('should query utilsRouteSvc to get routes', function() {
+    form.test.$setViewValue('Route')
+    scope.$digest()
+
+    expect(utilsRouteSvc.getRoutes).toHaveBeenCalled()
+  })
+
+  it('should validate the form if name is unique', function() {
+    var routes = [{ name: 'Route2' }]
+
+    form.test.$setViewValue('Route1')
+    deferred.resolve(routes)
+    scope.$digest()
+
+    expect(scope.model.test).toEqual('Route1')
+    expect(form.test.$valid).toBe(true)
+  })
+
+  it('should not validate the form if name exists', function() {
+    var routes = [{ name: 'Route1' }]
+
+    form.test.$setViewValue('Route1')
+    deferred.resolve(routes)
+    scope.$digest()
+
+    expect(scope.model.test).toBeUndefined()
+    expect(form.test.$valid).toBe(false)
+  })
+
+
+  it('should validate the form if routes are not available', function() {
+    form.test.$setViewValue('Route1')
+    deferred.reject()
+    scope.$digest()
+
+    expect(scope.model.test).toEqual('Route1')
+    expect(form.test.$valid).toBe(true)
+  })
+})
